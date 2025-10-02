69a88a416c431487a77cc994c4445d1a09069a36
rda12345 <roiedann1987@gmail.com>
2025-10-02 23:03:01 +0300

    renamed files

--- code/Taxi-v3.py
+++ code/Taxi-v3.py
@@ -22,7 +22,7 @@ Solution - solved with policy and value iteration methods.
 import numpy as np
 import gymnasium as gym
 import matplotlib.pyplot as plt
-from DPAgent import DPAgent
+from dynamical_programming import DPAgent
 
 
 ## Model parameters
